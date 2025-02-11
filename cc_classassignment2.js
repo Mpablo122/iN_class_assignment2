d3105211d2588df08d66a981e575698a1d8f1393
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-02-11 16:38:00 -0500

    full commit

--- cc_classassignment2.js
+++ cc_classassignment2.js
@@ -5,44 +5,44 @@ let products = [
     {name: "Banana", price: 2.00, category: "Produce"}
 ]
 
-// function getProductsByCategory(products, category) {
-//    return products.filter(product => product.category === category)
-// };
+function getProductsByCategory(products, category) {
+   return products.filter(product => product.category === category)
+};
 
-// console.log(getProductsByCategory(products, "Electronics"));
+console.log(getProductsByCategory(products, "Electronics"));
 
 // Task 2
 
-// function applydicount(products, discountRate){
-//     return products.map(product => ({
-//         ...product,
-//         price: product.price - (product.price * discountRate)
-//     }
+function applydicount(products, discountRate){
+    return products.map(product => ({
+        ...product,
+        price: product.price - (product.price * discountRate)
+    }
 
-//     ))
-// }
+    ))
+}
 
-// console.log(applydicount(products,.1))
+console.log(applydicount(products,.1))
 
-//task 3 
+// task 3 
 
-// let sales = [ 250, 400, 150, 900, 1200]
+let sales = [ 250, 400, 150, 900, 1200]
 
-// function calculateTotalRevenue(sales){
-//     return sales.reduce((total, sale) => total + sale, 0)
-// }
+function calculateTotalRevenue(sales){
+    return sales.reduce((total, sale) => total + sale, 0)
+}
 
-// console.log(calculateTotalRevenue(sales))
+console.log(calculateTotalRevenue(sales))
 
-//Task 4
-// let employee = {
-//     name: "John doe", 
-//     salary: 50000,
-//     position: "manager" 
-// };
+// Task 4
+let employee = {
+    name: "John doe", 
+    salary: 50000,
+    position: "manager" 
+};
 
-// function updateSalary(employee, percentageIncrease){
-//     return employee.salary += employee.salary + percentageIncrease
-// }
+function updateSalary(employee, percentageIncrease){
+    return employee.salary += employee.salary + percentageIncrease
+}
 
-// console.log(updateSalary(employee, 0.1))
+console.log(updateSalary(employee, 0.1))
